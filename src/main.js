ab4c07d64f2e747179edbb327465ace70003d3b3
杨腾祺 <3150475761@qq.com>
2020-06-22 19:29:15 +0800

    10-追加第三方库axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vant from 'vant';
+import axios from 'axios'
 import 'vant/lib/index.css';
-
+Vue.prototype.$axios = axios
 Vue.use(Vant);
 Vue.config.productionTip = false
 
