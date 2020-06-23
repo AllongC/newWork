d26ffab4a00267930d0fed5752676f4c3d40a4e5
杨腾祺 <3150475761@qq.com>
2020-06-23 18:25:42 +0800

    15-注册页面功能完成

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,15 +5,15 @@
       type="text"
       InputText="用户名/手机号码"
       errMsg="手机号码格式不正确"
-      rule="^\d{3,6}$"
+      :rule="ruleName"
       @sendData="setUsername"
     />
-    <InputSec type="text" InputText="昵称" errMsg="昵称格式不正确" rule="^\w{3,6}$" @sendData="setNickname" />
+    <InputSec type="text" InputText="昵称" errMsg="昵称格式不正确" :rule="ruleNick" @sendData="setNickname" />
     <InputSec
       type="password"
       InputText="密码"
       errMsg="密码格式不正确"
-      rule="^\d{3,6}$"
+      :rule="rulePwd"
       @sendData="setPassword"
     />
     <LoginSec BtnInfo="注册" @send="accept" />
@@ -29,7 +29,10 @@ export default {
     return {
       username: "",
       nickname: "",
-      password: ""
+      password: "",
+      ruleName: "^\\d{3,6}$",
+      rulePwd: "^\\d{3,6}$",
+      ruleNick: "^\\w{3,6}$"
     };
   },
   methods: {
@@ -43,10 +46,41 @@ export default {
       this.password = newVal;
     },
     accept() {
-      console.log(this.username);
-      console.log(this.nickname);
-      console.log(this.password);
-      console.log("父组件触发...");
+      if (this.username == "" || this.nickname == "" || this.password == "") {
+        this.$toast.fail("输入不能为空！");
+        return;
+      }
+      let reg = new RegExp(this.ruleName).test(this.username);
+      if (!reg) {
+        this.$toast.fail("手机号码格式不正确!");
+        return;
+      }
+      reg = new RegExp(this.ruleNick).test(this.nickname);
+      if (!reg) {
+        this.$toast.fail("昵称格式不正确!");
+        return;
+      }
+      reg = new RegExp(this.rulePwd).test(this.password);
+      if (!reg) {
+        this.$toast.fail("密码格式不正确!");
+        return;
+      }
+      this.$axios({
+        url: "http://127.0.0.1:3000/register",
+        method: "post",
+        data: {
+          username: this.username,
+          password: this.password,
+          nickname: this.nickname
+        }
+      }).then(res => {
+        const { message } = res.data;
+        if (message == "注册成功") {
+          this.$toast.success(message);
+        } else {
+          this.$toast.fail(message);
+        }
+      });
     }
   },
   components: {
