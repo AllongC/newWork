d936ed2d187750294de2af957c3633f9dcbf2326
杨腾祺 <3150475761@qq.com>
2020-06-25 11:51:56 +0800

    18-axios拦截

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import router from './router'
 import Vant from 'vant';
 import axios from 'axios'
 import 'vant/lib/index.css';
-Vue.prototype.$axios = axios
 import { Toast } from 'vant'
 Vue.use(Vant);
+axios.interceptors.response.use(res => {
+  const { statusCode, message } = res.data
+  if (statusCode && statusCode == 401) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    Toast.fail(message)
+    router.replace('/login')
+  }
+  return res
+})
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 router.beforeEach((to, from, next) => {
   if (to.name == 'Center') {
