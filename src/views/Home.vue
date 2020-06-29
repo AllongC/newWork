1c323d79dd5a7593f474b60f320825083061f21b
杨腾祺 <3150475761@qq.com>
2020-06-29 23:54:37 +0800

    30-无限加载列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,15 @@
     <HomeTop />
     <van-tabs v-model="current" bind:change="onChange">
       <van-tab v-for="item in header" :key="item.id" :title="item.name">
-        <HomeCategory v-for="item in category" :key="item.id" :category="item" />
+        <van-list
+          v-model="item.loading"
+          :finished="item.finished"
+          :immediate-check="false"
+          finished-text="我也是有底线的"
+          @load="loadMore"
+        >
+          <HomeCategory v-for="post in item.category" :key="post.id" :category="post" />
+        </van-list>
       </van-tab>
     </van-tabs>
   </div>
@@ -16,8 +24,7 @@ export default {
   data() {
     return {
       current: 0,
-      header: [],
-      category: []
+      header: []
     };
   },
   methods: {
@@ -26,22 +33,44 @@ export default {
         url: "/category"
       }).then(res => {
         const { data } = res.data;
-        this.header = data;
+        this.header = data.map(item => {
+          return {
+            ...item,
+            category: [],
+            pageIndex: 1,
+            pageSize: 5,
+            finished: false,
+            loading: false
+          };
+        });
         this.getCategory();
       });
     },
 
     getCategory() {
+      const item = this.header[this.current];
       this.$axios({
         url: "/post",
         methods: "get",
         params: {
-          category: this.getId()
+          category: this.getId(),
+          pageIndex: item.pageIndex,
+          pageSize: item.pageSize
         }
       }).then(res => {
         const { data } = res.data;
-        this.category = data;
+        item.category = [...item.category, ...data];
+        console.log(item.category);
       });
+    },
+
+    loadMore() {
+      const item = this.header[this.current];
+      item.pageIndex += 1;
+      this.getCategory();
+      if (item.category.length <= item.pageSize) {
+        item.finished = true;
+      }
     },
 
     getId() {
@@ -50,7 +79,9 @@ export default {
   },
   watch: {
     current() {
-      this.update();
+      if (this.header[this.current].category.length == 0) {
+        this.getCategory();
+      }
     }
   },
   mounted() {
